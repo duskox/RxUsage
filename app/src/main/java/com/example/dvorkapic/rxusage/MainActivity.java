541fe59cdb6cbb10921047d8d7cc3c6024b83822
Dusko <duskox@gmail.com>
2016-03-31 16:36:33 +0200

    Rx calls implemented

--- app/src/main/java/com/example/dvorkapic/rxusage/MainActivity.java
+++ app/src/main/java/com/example/dvorkapic/rxusage/MainActivity.java
@@ -1,13 +1,99 @@
 package com.example.dvorkapic.rxusage;
 
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.dvorkapic.rxusage.Pojo.CurrentWeatherData;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import retrofit2.Retrofit;
+import rx.Observable;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    @Bind(R.id.et_city)
+    EditText etCity;
+
+    @Bind(R.id.tv_temperature)
+    TextView tvTemperature;
+
+    @Bind(R.id.tv_humidity)
+    TextView tvHumidity;
+
+    @Bind(R.id.tv_pressure)
+    TextView tvPRessure;
+
+    @Bind(R.id.tv_sunrise)
+    TextView tvSunrise;
+
+    @Bind(R.id.tv_sunset)
+    TextView tvSunset;
+
+    @Bind(R.id.srl_refresh)
+    SwipeRefreshLayout srlRefresh;
+
+    Observable<CurrentWeatherData> observable;
+    WeatherService weatherService;
+
+    private final String apiKey = "9330706858110721d507c1057cae7b61";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        srlRefresh.setOnRefreshListener(this);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.openweathermap.org/data/2.5/")
+                .build();
+
+
+        weatherService = retrofit.create(WeatherService.class);
+
+
+
+
+    }
+
+    @Override
+    public void onRefresh() {
+        observable =  weatherService.getCurrentWeatherData(etCity.getText().toString(), apiKey);
+        observable.subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<CurrentWeatherData>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(CurrentWeatherData currentWeatherData) {
+                        tvHumidity.setText(currentWeatherData.getMainInfo().getTemp().toString());
+                        tvHumidity.setText(currentWeatherData.getMainInfo().getHumidity().toString());
+                        tvPRessure.setText(currentWeatherData.getMainInfo().getPressure().toString());
+                        tvSunrise.setText(currentWeatherData.getSys().getSunrise());
+                        tvSunset.setText(currentWeatherData.getSys().getSunset());
+                    }
+                });
+    }
+
+    private void showSnack(String message) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/dvorkapic/rxusage/WeatherService.java
+++ app/src/main/java/com/example/dvorkapic/rxusage/WeatherService.java
@@ -5,11 +5,12 @@ import com.example.dvorkapic.rxusage.Pojo.CurrentWeatherData;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * Created by dvorkapic on 31/03/16.
  */
 public interface WeatherService {
     @GET("weather")
-    Call<CurrentWeatherData> getCurrentWeatherData(@Query("q") String city);
+    Observable<CurrentWeatherData> getCurrentWeatherData(@Query("q") String city, @Query("appId") String apiKey);
 }
